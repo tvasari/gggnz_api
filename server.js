4cfd38fb703fd090b083ec9bbfc0cfc75da1a9b3
Tommaso <tommaso.vsr@gmail.com>
2020-05-29 17:09:32 +0200

    added database

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-elliptical-39670',
     user : 'postgres',
     password : 'test',
     database : 'gggnz'
