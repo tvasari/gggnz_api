86d44a75cd17a887b20bb9f2bb49b191b26557fc
Tommaso <tommaso.vsr@gmail.com>
2020-06-30 10:07:07 +0200

    adding mysql connection

--- app.js
+++ app.js
@@ -5,34 +5,36 @@ const knex = require('knex');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
+    client: 'mysql',
+    connection: {
+        host: 'mysql.giuseppegnozzi.xyz',
+        user: 'gggnz',
+        password: '7KGYs98XfiBDr4f',
+        database: 'gggnz'
+    }
 });
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {res.send('It is working');})
+app.get('/', (req, res) => { res.send('It is working'); })
 
 app.get('/homecards', (req, res) => {
-  db.select('*').from('homecards').then(data => {
-    res.send(data);
-  });
+    db.select('*').from('homecards').then(data => {
+        res.send(data);
+    });
 })
 
 app.get('/workscards', (req, res) => {
-  db.select('*').from('workscards').then(data => {
-    res.send(data);
-  });
+    db.select('*').from('workscards').then(data => {
+        res.send(data);
+    });
 })
 
 app.get('/sfondignz', (req, res) => {
-  db.select('url').from('sfondignz').then(data => {
-    res.send(data);
-  });
+    db.select('url').from('sfondignz').then(data => {
+        res.send(data);
+    });
 })
 
-app.listen(80, console.log(`listening to port 80`));
+app.listen(3306, console.log(`listening to port 3306`));
